2e5317dd8f8b88a1375f1b9dfa4c7fc478756785
Knrc <kontakt.mikey@gmail.com>
2023-09-13 12:14:09 +0200

    Dealer can Deal.exe and Player can Play.exe

--- Assets/Scripts/BlackJack.cs
+++ Assets/Scripts/BlackJack.cs
@@ -1,11 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BlackJack : MonoBehaviour
 {
+    public TMP_Text playerText;
+    public TMP_Text dealerText;
+    private int _score = 0;
+    private int _dealerScore;
+
+    private void Start()
+    {
+        DealerDrawCard();
+    }
+
+    public void DealerDrawCard()
+    {
+        _dealerScore = Random.Range(17, 27);
+
+        dealerText.text = "Dealer: " + _dealerScore;
+    }
+    
     public void DrawCard()
     {
-        int drawnCard = Random.Range(0, 22);
+        int drawnCard = Random.Range(1, 11);
+
+        _score = _score + drawnCard;
+        
+        playerText.text = "Player: " + (_score).ToString();
+    }
+
+    public void CompareValues()
+    {
+        
     }
 }
